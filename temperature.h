4920215d3694684e1c5583ae422b054bf13aff81
Jose Yepez <yejo32602@gmail.com>
2026-05-29 00:03:34 +0000

    Initial Commit

---
+++ temperature.h
@@ -0,0 +1,10 @@
+# ifndef temperature_h
+# define temperature_h
+
+int celsius_to_fahrenheit_int(int c);
+double celsius_to_fahrenheit(double c);
+int fahrenheit_to_celsius_int(int f);
+double fahrenheit_to_celsius(double f);
+void print_conversion_table(int start, int end, int step);
+
+# endif
